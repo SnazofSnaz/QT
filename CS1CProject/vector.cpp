fca71a284a621b45ecfb0deb24a75cba105d0cf6
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2020-05-11 13:21:08 -0700

    Update vector.cpp

--- CS1CProject/vector.cpp
+++ CS1CProject/vector.cpp
@@ -6,7 +6,7 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include "vector_doubles.h"
+#include "vector.h"
 using namespace myStd;
 
 #include <iostream>
